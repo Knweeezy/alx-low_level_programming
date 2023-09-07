56657ac2bb1176c3380ffb8ec1c3fa7289c97175
Knweeezy <knweeezy@gmail.com>
2023-09-07 07:32:15 -0700

    task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Descrition : a program that will print a line using puts function
+ * Return : 0 when success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
