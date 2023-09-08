bf459c29cdabb50f35a1df954acd4e7b6985936e
Knweeezy <knweeezy@gmail.com>
2023-09-08 06:35:11 -0700

    task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program prints number from 0 to 9
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
