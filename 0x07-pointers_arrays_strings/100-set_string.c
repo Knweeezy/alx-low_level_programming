fefaa13e9d92f0e7600e06be99d32172f4f95d8f
Knweeezy <knweeezy@gmail.com>
2023-09-25 13:04:57 -0700

    task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - fuction of start
+ * @s: action
+ * @to: action
+ * Return: Always 0 (success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
